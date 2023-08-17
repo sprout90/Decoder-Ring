49f52015d2e40973143d66ed3925c0a558f06b63
sprout90 <mikebrehm420@comcast.net>
2023-08-17 17:05:51 -0500

    First draft of substitution tests

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,12 +1,12 @@
 const { expect } = require("chai");
 const { substitution } = require("../src/substitution");
 
-const englishAlphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const englishAlphabet =      ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const substitutionAlphabet = ["x","o","y","q","m","c","g","r","u","k","s","w","a","f","l","n","t","h","d","j","p","z","i","b","e","v"]
 const message = "Get some beer and fresh popcorn!"
 
 describe("Substitution", () => {
-  describe("substition input validation", () => {
+  describe("input validation", () => {
     it("should return false if input is not a string", () => {
       
       const input = {name: "test object"};
@@ -32,6 +32,40 @@ describe("Substitution", () => {
         testAlpha.push("a");
         const answer = substitution(input, testAlpha)
         expect(answer).to.be.false;
+    });
+  });
+  describe("correct output tests", () => {
+    it("should return correct _encoded_ ouput for input message", () => {
+      
+      const input = message;
+      const expectedOutput = "gmj dlam ommh xfq chmdr nlnylhf!"
+      const answer = substitution(input, substitutionAlphabet);
+      expect(answer).to.equal(expectedOutput)
+    });
+    it("should return correct _decoded_ ouput for input message", () => {
+      
+      const input = "gmj dlam ommh xfq chmdr nlnylhf!"
+      const expectedOutput = message;
+      const answer = substitution(input, substitutionAlphabet, false);
+      expect(answer).to.equal(expectedOutput);
+    });
+    it("should maintain spaces from the input message", () => {
+      
+      const spacesMatch = true;
+      let foundSpaceIndex = 0;
+      let startIndex = 0;
+      const input = message;
+      const inputSpaceIndexes = [3,8,13,17,23]
+      const answer = substitution(input, substitutionAlphabet, false);
+
+      for (i=0; i<inputSpaceIndexes.length; i++){
+        foundSpaceIndex = answer.indexOf(" ", startIndex);
+        if (foundSpaceIndex != inputSpaceIndexes[i]) {
+          spacesMatch = false;
+          break;
+        }
+      }
+      expect(spacesMatch).to.be.true;
     });
   });
 });
